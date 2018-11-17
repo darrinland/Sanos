35adfdbd2309cc0db1c11f3de5c458cf7ce5a408
captainReed <fearlessleader2.0@gmail.com>
2018-11-17 01:50:03 -0700

    content in place

--- public/scripts/emotions.js
+++ public/scripts/emotions.js
@@ -11,7 +11,6 @@ let startemotions = [
 emotions.push({
 	emotionLabel: "Happy",
 	startFromAny: true,
-	color: "0xffff",
 	startFromEmotions: [],
 	content: {
 		music: [
@@ -34,6 +33,19 @@ emotions.push({
 			{
 				label: " Hakuna Matata",
 				url: "https://www.youtube.com/watch?v=D4zG1Tnt5A8"
+			},
+			{
+				label: " Life is a Happy Song from the Muppets",
+				url:
+					"https://www.youtube.com/watch?v=MEirVGWyD4s&index=3&list=PL753B4EE6BF24569D"
+			},
+			{
+				label: " Africa by Toto",
+				url: "https://www.youtube.com/watch?v=FTQbiNvZqaY"
+			},
+			{
+				label: " Dont Worry Be Happy",
+				url: "https://www.youtube.com/watch?v=d-diB65scQU"
 			}
 		],
 		video: [
@@ -57,12 +69,77 @@ emotions.push({
 			{
 				label: " helium chior kid",
 				url: "https://www.youtube.com/watch?v=29N-CoSGP2c"
+			},
+			{
+				label: " Artic fox laughing",
+				url:
+					"https://www.youtube.com/watch?v=-W9xKvQl2CQ&feature=youtu.be&fbclid=IwAR3CcNGBMJcPbejQZqTcdLWyJI8VmTPW4Bv4bHRYXhslDg6fPutlt1-mJbM"
+			},
+			{
+				label: " The Nicest Place on the Internet",
+				url:
+					"https://www.youtube.com/watch?v=HZ-IhHzaOwA&list=PL8D2A19E981586AC2&index=2"
+			},
+			{
+				label: " Funny cat videos",
+				url: "https://www.youtube.com/watch?v=XyNlqQId-nk"
+			},
+			{
+				label: " You is Kind ",
+				url: "https://www.youtube.com/watch?v=3H50llsHm3k"
 			}
 		],
 		website: [
-			{ label: " Good News", url: "https://www.today.com/news/good-news" }
+			{ label: " Good News", url: "https://www.today.com/news/good-news" },
+			{
+				label: " 1000 awesome things ",
+				url: "http://1000awesomethings.com/?cnn=yes"
+			},
+			{
+				label: " Online cognitive based therapy",
+				url: "https://www.happify.com/"
+			},
+			{ label: " Pug licking screen", url: "http://www.sanger.dk/" },
+			{ label: " Chicken on a Raft", url: "https://chickenonaraft.com/" },
+			{
+				label: " A button that makes everything okay",
+				url: "http://make-everything-ok.com/"
+			},
+			{
+				label: " Make people smile project ",
+				url: "https://www.youtube.com/watch?v=8YuBxP4CKZc"
+			},
+			{
+				label: " Live cam of Disneyland",
+				url:
+					"https://www.webcamtaxi.com/en/usa/california/anaheim-disneyland.html?fbclid=IwAR1N3uOGS16Czv81ItTZNUr3pfCtjR6LoKzz2_ixIBaZhv0tRoNjyksUYhY"
+			},
+			{
+				label: " Students Against Depression",
+				url: "https://www.studentsagainstdepression.org/"
+			},
+			{
+				label: " Faith in Humanity Stories ",
+				url:
+					"https://www.success.com/29-beautiful-stories-that-will-restore-your-faith-in-humanity/"
+			},
+			{ label: " Buy a piece of Ireland", url: "https://www.buyireland.com/" }
 		],
-		activity: []
+		activity: [
+			{
+				label: " Go outside. Breathe the fresh air-- even if it's for 2 min. ",
+				url: ""
+			},
+			{ label: " Write a thank you note to someone you care about. ", url: "" },
+			{ label: " Buy some seeds to plant. Take care of the plant. ", url: "" },
+			{ label: " Call up a friend", url: "" },
+			{ label: " Buy a fish to take care of", url: "" },
+			{ label: " Go run as fast as you can for as long as you can", url: "" },
+			{ label: " Drink a glass of cold water ", url: "" },
+			{ label: " Give someone a hug", url: "" },
+			{ label: " Shower ", url: "" },
+			{ label: " Make your bed ", url: "" }
+		]
 	}
 });
 
@@ -71,10 +148,126 @@ emotions.push({
 	startFromAny: true,
 	startFromEmotions: [],
 	content: {
-		music: [],
-		video: [],
-		website: [],
-		activity: []
+		music: [
+			{
+				label: " I love my shirt",
+				url: "https://www.youtube.com/watch?v=miAVhZ6rKFo"
+			},
+			{
+				label: " Eye of the Tiger",
+				url: "https://www.youtube.com/watch?v=QEjgPh4SEmU"
+			},
+			{
+				label: " Don't Stop Believin ",
+				url: "https://www.youtube.com/watch?v=1k8craCGpgs"
+			},
+			{
+				label: " Wavin Flag",
+				url: "https://www.youtube.com/watch?v=WTJSt4wP2ME"
+			},
+			{
+				label: " not throwing away my shot",
+				url: "https://www.youtube.com/watch?v=Ic7NqP_YGlg"
+			},
+			{
+				label: " Cant Hold Us",
+				url: "https://www.youtube.com/watch?v=NfOPFcNJPVM"
+			},
+			{
+				label: " Whatever Ti Takes",
+				url: "https://www.youtube.com/watch?v=gOsM-DYAEhY"
+			}
+		],
+		video: [
+			{
+				label: " Invincible motivational video",
+				url: "https://www.youtube.com/watch?v=zIFVwF35Jb0"
+			},
+			{
+				label: " What's so Funny About Mental Illness",
+				url:
+					"https://www.youtube.com/watch?v=mbbMLOZjUYI&list=PLs1f8FLNPv3X_zvmigytXCEaZVXFa973J&index=4"
+			},
+			{
+				label: " The Power of Vulnerability",
+				url:
+					"https://www.youtube.com/watch?v=iCvmsMzlF7o&list=PLs1f8FLNPv3X_zvmigytXCEaZVXFa973J&index=8"
+			},
+			{
+				label: " You is Kind ",
+				url: "https://www.youtube.com/watch?v=3H50llsHm3k"
+			},
+			{
+				label: " Rocky Training",
+				url: "https://www.youtube.com/watch?v=ABg0c_E7OOI"
+			},
+			{
+				label: " Power of Motivation",
+				url: "https://www.youtube.com/watch?v=9hdSLiHaJz8"
+			}
+		],
+		website: [
+			{
+				label: " Emergency Compliment ",
+				url: "http://emergencycompliment.com/"
+			},
+			{
+				label: " Annonymously post a secret you've been harboring ",
+				url: "https://postsecret.com/?cnn=yes"
+			},
+			{
+				label: " A button that makes everything okay",
+				url: "http://make-everything-ok.com/"
+			},
+			{ label: " Educate yourself!", url: "https://www.khanacademy.org/" },
+			{
+				label: " DO IT by Shia Labouf ",
+				url: "https://www.youtube.com/watch?v=ZXsQAXx_ao0"
+			},
+			{
+				label: " A website full of location based opportunities to serve",
+				url: "https://www.justserve.org/"
+			},
+			{
+				label: " Students Against Depression",
+				url: "https://www.studentsagainstdepression.org/"
+			}
+		],
+		activity: [
+			{
+				label: " Write down a goal you can accomplish today. Then do it. ",
+				url: ""
+			},
+			{
+				label:
+					" Write down a goal you want to accomplish in the next week. Do it. ",
+				url: ""
+			},
+			{
+				label:
+					" Write down a goal you want to accomplish in the next month. Do it. ",
+				url: ""
+			},
+			{
+				label:
+					" Climb to the top of a high spot and scream at the top of your lungs ",
+				url: ""
+			},
+			{
+				label:
+					" Remember that project you promised you would finish? Finish it. ",
+				url: ""
+			},
+			{
+				label:
+					" Put on Aviators and listen to this: https://www.youtube.com/watch?v=_DNUv3QfqyM&index=1&list=PL493TQZToFwGtwMh1_9Y_nuralywpdQ45",
+				url: ""
+			},
+			{ label: " Learn something new", url: "" },
+			{ label: " Choose a hobby to improve on", url: "" },
+			{ label: " Make your bed ", url: "" },
+			{ label: " Eat a Carrot (or another vegetable of choice)", url: "" }
+		]
 	}
 });
 
@@ -83,10 +276,89 @@ emotions.push({
 	startFromAny: true,
 	startFromEmotions: [],
 	content: {
-		music: [],
-		video: [],
-		website: [],
-		activity: []
+		music: [
+			{
+				label: " wiskey lulliby",
+				url: "https://www.youtube.com/watch?v=IZbN_nmxAGk"
+			},
+			{
+				label: " My Heart Will Go On ",
+				url: "https://www.youtube.com/watch?v=DNyKDI9pn0Q"
+			},
+			{
+				label: " in the arms of the angles",
+				url: "https://www.youtube.com/watch?v=9gspElv1yvc"
+			},
+			{
+				label: " i am one with the force",
+				url: "https://www.youtube.com/watch?v=FXcZ-4cPRhw"
+			},
+			{
+				label: " lil sebastian",
+				url: "https://www.youtube.com/watch?v=k1jUTCH-83U"
+			},
+			{
+				label: " christmas shoes",
+				url: "https://www.youtube.com/watch?v=1i9G60wvH7Y"
+			},
+			{
+				label: " You Raise Me Up by Josh Groban",
+				url: "https://www.youtube.com/watch?v=aJxrX42WcjQ"
+			}
+		],
+		video: [
+			{
+				label: " Chistian the Lion",
+				url: "https://www.youtube.com/watch?v=btuxO-C2IzE"
+			},
+			{
+				label: " The first four minutes of UP",
+				url: "https://www.youtube.com/watch?v=F2bk_9T482g"
+			},
+			{
+				label: " how come he dont want me",
+				url: "https://www.youtube.com/watch?v=xIHscdMAG4c"
+			},
+			{
+				label: " 3rd Grade Dropout",
+				url: "https://www.youtube.com/watch?v=Bg_Q7KYWG1g"
+			},
+			{
+				label: " little foots mom dies",
+				url: "https://www.youtube.com/watch?v=WbW8GgAWKi8"
+			},
+			{
+				label: " Feeling all the feels!",
+				url: "https://www.youtube.com/watch?v=gAMbkJk6gnE"
+			}
+		],
+		website: [
+			{
+				label: " The Nicest Place on the Internet",
+				url:
+					"https://www.youtube.com/watch?v=HZ-IhHzaOwA&list=PL8D2A19E981586AC2&index=2"
+			},
+			{
+				label: " Punch the Computer",
+				url:
+					"http://stressreliefpig.com/games/aggressive/destroy-the-computer.html"
+			},
+			{
+				label: " Faith in Humanity Restored",
+				url:
+					"https://www.boredpanda.com/faith-in-humanity-restored/?utm_source=google&utm_medium=organic&utm_campaign=organic"
+			}
+		],
+		activity: [
+			{
+				label: " Buy a bunch of glass plates. Throw them on the floor. ",
+				url: ""
+			},
+			{
+				label: " Listen to a song that makes you cry and allow yourself to cry",
+				url: ""
+			}
+		]
 	}
 });
 
@@ -95,10 +367,89 @@ emotions.push({
 	startFromAny: true,
 	startFromEmotions: [],
 	content: {
-		music: [],
-		video: [],
-		website: [],
-		activity: []
+		music: [
+			{
+				label: " Somewhere over the rainbow",
+				url:
+					"https://www.youtube.com/watch?v=V1bFr2SWP1I&fbclid=IwAR3yA4SOFQUM0QtYkXPg__YI9cNVCvOySQBIIvG6Lrp_2tcTi-atGvQbbOc&app=desktop#dialog"
+			},
+			{
+				label: " Calm Piano Music",
+				url: "https://www.youtube.com/watch?v=rLMHGjoxJdQ"
+			},
+			{
+				label: " Waves",
+				url: "https://www.youtube.com/watch?v=VH8ybWrLR8Q&t=9947s"
+			},
+			{
+				label: " Rain",
+				url:
+					"https://rainymood.com/?fbclid=IwAR0YlUXxGCgK3i0RXpbnwWG2ll8YwoVtCSu9MXDacg8Bv6N-tnpheD4d58o"
+			},
+			{ label: " White noise of your choice", url: "https://asoftmurmur.com/" },
+			{
+				label: " Relaxing Guitar",
+				url: "https://www.youtube.com/watch?v=j4OuCMnY43E"
+			}
+		],
+		video: [
+			{
+				label: " Live videos of Animals",
+				url:
+					"https://explore.org/livecams/explore-all-cams/bald-eagle-west-end-catalina"
+			},
+			{
+				label: " Mindfulness Ted Talk",
+				url: "https://www.youtube.com/watch?v=qzR62JJCMBQ"
+			},
+			{
+				label: " Oddly Satisfying compilation",
+				url: "https://www.youtube.com/watch?v=T1sa8uMvpp8"
+			},
+			{
+				label: " Happy Trees Bob Ross ",
+				url: "https://www.youtube.com/watch?v=oh5p5f5_-7A"
+			},
+			{
+				label: " Deep breathing ",
+				url: "https://www.youtube.com/watch?v=aXItOY0sLRY"
+			}
+		],
+		website: [
+			{
+				label: " See where you are in the universe",
+				url: "http://stars.chromeexperiments.com/"
+			},
+			{
+				label: " Do Nothing for 2 Minutes Straight",
+				url: "http://www.donothingfor2minutes.com/"
+			},
+			{ label: " online journal", url: "https://penzu.com/" },
+			{
+				label:
+					" Treat yourself by picking up a good book! Here, have some recommendations! ",
+				url: "https://www.recommendmeabook.com/"
+			},
+			{
+				label: " A button that makes everything okay",
+				url: "http://make-everything-ok.com/"
+			},
+			{
+				label: " https://www.studentsagainstdepression.org/",
+				url:
+					"https://wiredforhappy.com/100-smart-ways-to-calm-your-anxious-mind/"
+			}
+		],
+		activity: [
+			{
+				label:
+					" Draw a picture of how you are feeling. Don't worry about it looking good.",
+				url: ""
+			},
+			{ label: " Take a hot shower", url: "" },
+			{ label: " Go on a walk ", url: "" },
+			{ label: " Take deep breaths for 30 seconds ", url: "" }
+		]
 	}
 });
 
@@ -107,10 +458,70 @@ emotions.push({
 	startFromAny: true,
 	startFromEmotions: [],
 	content: {
-		music: [],
-		video: [],
-		website: [],
-		activity: []
+		music: [
+			{
+				label: " Somewhere over the rainbow",
+				url:
+					"https://www.youtube.com/watch?v=V1bFr2SWP1I&fbclid=IwAR3yA4SOFQUM0QtYkXPg__YI9cNVCvOySQBIIvG6Lrp_2tcTi-atGvQbbOc&app=desktop#dialog"
+			},
+			{
+				label: " For You Now by Bruno Merz",
+				url: "https://www.youtube.com/watch?v=HKu9EK5N4qw"
+			},
+			{
+				label: " I Won't Give Up by Jason Mraz",
+				url:
+					"https://www.youtube.com/watch?v=O1-4u9W-bns&index=16&list=PLhGO2bt0EkwvRUioaJMLxrMNhU44lRWg8"
+			},
+			{
+				label: " they can never take our freedom",
+				url: "https://www.youtube.com/watch?v=gr_OpFxCx-A"
+			},
+			{
+				label: " You Raise Me Up by Josh Groban ",
+				url: "https://www.youtube.com/watch?v=aJxrX42WcjQ"
+			}
+		],
+		video: [
+			{
+				label: " You is Kind ",
+				url: "https://www.youtube.com/watch?v=3H50llsHm3k"
+			},
+			{
+				label: " Hope Motivational",
+				url: "https://www.youtube.com/watch?v=Ie-ByDkfxG8"
+			}
+		],
+		website: [
+			{
+				label: " The Nicest Place on the Internet",
+				url:
+					"https://www.youtube.com/watch?v=HZ-IhHzaOwA&list=PL8D2A19E981586AC2&index=2"
+			},
+			{
+				label: " A button that makes everything okay",
+				url: "http://make-everything-ok.com/"
+			},
+			{
+				label: " The Power of Vulnerability ",
+				url:
+					"https://www.youtube.com/watch?v=iCvmsMzlF7o&list=PLs1f8FLNPv3X_zvmigytXCEaZVXFa973J&index=8"
+			},
+			{
+				label: " Students Against Depression",
+				url: "https://www.studentsagainstdepression.org/"
+			},
+			{
+				label: " Faith in Humanity Restored ",
+				url:
+					"https://www.boredpanda.com/faith-in-humanity-restored/?utm_source=google&utm_medium=organic&utm_campaign=organic"
+			}
+		],
+		activity: [
+			{ label: " pay for a strangers lunch", url: "" },
+			{ label: " leave a bunch of positive yelp reviews", url: "" },
+			{ label: " adopt a highway", url: "" }
+		]
 	}
 });
 
@@ -119,9 +530,68 @@ emotions.push({
 	startFromAny: true,
 	startFromEmotions: [],
 	content: {
-		music: [],
-		video: [],
-		website: [],
+		music: [
+			{
+				label: " Rainbow Connection",
+				url: "https://www.youtube.com/results?search_query=rainbow+connection"
+			},
+			{
+				label: " we are the world",
+				url: "https://www.youtube.com/watch?v=M9BNoNFKCBI"
+			},
+			{
+				label: " i wont give up on us",
+				url: "https://www.youtube.com/watch?v=0cNhpIzUreI"
+			}
+		],
+		video: [
+			{
+				label: " The Power of Vulnerability ",
+				url:
+					"https://www.youtube.com/watch?v=iCvmsMzlF7o&list=PLs1f8FLNPv3X_zvmigytXCEaZVXFa973J&index=8"
+			},
+			{
+				label: " The Hidden Truth About Human Connection",
+				url: "https://www.youtube.com/watch?v=23vBLxJQk-Y"
+			},
+			{
+				label: " The Nicest Place on the Internet",
+				url:
+					"https://www.youtube.com/watch?v=HZ-IhHzaOwA&list=PL8D2A19E981586AC2&index=2"
+			},
+			{
+				label: " How to Make Friends When You Are Socially Awkward",
+				url: "https://www.youtube.com/watch?v=MP5CkokeygQ"
+			}
+		],
+		website: [
+			{
+				label: " The Nicest Place on the Internet",
+				url:
+					"https://www.youtube.com/watch?v=HZ-IhHzaOwA&list=PL8D2A19E981586AC2&index=2"
+			},
+			{
+				label: " 56 Random Acts of Kindness",
+				url:
+					"https://www.blessingmanifesting.com/2018/08/56-random-acts-of-kindness-ideas.html/"
+			},
+			{
+				label: " Vent to non-judgmental people on the internet!",
+				url: "https://blahtherapy.com/"
+			},
+			{
+				label: " Feeling lonely? Connect with other lonely people.",
+				url: "http://www.webofloneliness.com/"
+			},
+			{
+				label: " Stories of people in New York",
+				url: "http://www.humansofnewyork.com/"
+			},
+			{
+				label: " A website full of location based opportunities to serve",
+				url: "https://www.justserve.org/"
+			}
+		],
 		activity: []
 	}
 });
@@ -131,9 +601,59 @@ emotions.push({
 	startFromAny: true,
 	startFromEmotions: [],
 	content: {
-		music: [],
-		video: [],
-		website: [],
+		music: [
+			{
+				label: " the garden of your mind",
+				url: "https://www.youtube.com/watch?v=OFzXaFbxDcM"
+			},
+			{
+				label: " Inspirational Music",
+				url: "https://www.youtube.com/watch?v=c9V3FBJ8FoA"
+			}
+		],
+		video: [
+			{
+				label: " diy life hacks",
+				url: "https://www.youtube.com/watch?v=9UCny7hCeiI"
+			},
+			{
+				label: " life hacks 2",
+				url: "https://www.youtube.com/watch?v=CdLQ-mJi7rU"
+			},
+			{
+				label: " how to be creative",
+				url: "https://www.youtube.com/watch?v=zO2LdDpx-Tc"
+			},
+			{
+				label: " How to boost creativity",
+				url: "https://www.youtube.com/watch?v=_NHu6gM91aA"
+			},
+			{
+				label: " How to Widdle",
+				url: "https://www.youtube.com/watch?v=EB3USnPADvQ"
+			},
+			{ label: " Bob Ross ", url: "https://www.youtube.com/user/BobRossInc" }
+		],
+		website: [
+			{ label: " online journal", url: "https://penzu.com/" },
+			{ label: " Create cool abstract art", url: "http://weavesilk.com/" },
+			{
+				label:
+					" This website gives you a one word prompt that you must respond to. Drop the stress, don't think, and just write. ",
+				url: "https://www.oneword.com/"
+			},
+			{
+				label:
+					" Treat yourself by picking up a good book! Here, have some recommendations! ",
+				url: "https://www.recommendmeabook.com/"
+			},
+			{ label: " instructables", url: "https://www.instructables.com/" },
+			{
+				label: " Create blackout poetry",
+				url:
+					"https://www.powerpoetry.org/actions/5-tips-creating-blackout-poetry"
+			}
+		],
 		activity: []
 	}
 });
