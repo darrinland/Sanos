2630696e3e131ae27d4be42a4488cb203c05d8b7
Darrin Landroche <darrinland@gmail.com>
2018-11-16 18:31:23 -0700

    add emotions.js

---
+++ public/scripts/emotions.js
@@ -0,0 +1,33 @@
+let emotions = [
+	"Relief",
+	"Relaxed",
+	"Pleasant ",
+	"Peaceful",
+	"Joyful",
+	"In Control",
+	"Happy",
+	"Energized",
+	"Determined",
+	"Contempt",
+	"Connected",
+	"Collected",
+	"Cheerful",
+	"Certain"
+];
+
+let endEmotions = [
+	"Relief",
+	"Relaxed",
+	"Pleasant ",
+	"Peaceful",
+	"Joyful",
+	"In Control",
+	"Happy",
+	"Energized",
+	"Determined",
+	"Contempt",
+	"Connected",
+	"Collected",
+	"Cheerful",
+	"Certain"
+];
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,37 +1,3 @@
-let emotions = [
-	"Relief",
-	"Relaxed",
-	"Pleasant ",
-	"Peaceful",
-	"Joyful",
-	"In Control",
-	"Happy",
-	"Energized",
-	"Determined",
-	"Contempt",
-	"Connected",
-	"Collected",
-	"Cheerful",
-	"Certain"
-];
-
-let endEmotions = [
-	"Relief",
-	"Relaxed",
-	"Pleasant ",
-	"Peaceful",
-	"Joyful",
-	"In Control",
-	"Happy",
-	"Energized",
-	"Determined",
-	"Contempt",
-	"Connected",
-	"Collected",
-	"Cheerful",
-	"Certain"
-];
-
 let filteredEndEmotions = endEmotions;
 
 window.onload = () => {
