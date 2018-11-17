9410925438819eb8f6013ee3c7414c1c28aaf435
Darrin Landroche <darrinland@gmail.com>
2018-11-16 18:21:09 -0700

    Add JS rendering for workflow.

---
+++ public/scripts/main.js
@@ -0,0 +1,90 @@
+let emotions = [
+	"Relief",
+	"Relaxed",
+	"Pleasant ",
+	"Peaceful",
+	"Joyful",
+	"In Control",
+	"Happy",
+	"Energized",
+	"Determined",
+	"Contempt",
+	"Connected",
+	"Collected",
+	"Cheerful",
+	"Certain"
+];
+
+let endEmotions = [
+	"Relief",
+	"Relaxed",
+	"Pleasant ",
+	"Peaceful",
+	"Joyful",
+	"In Control",
+	"Happy",
+	"Energized",
+	"Determined",
+	"Contempt",
+	"Connected",
+	"Collected",
+	"Cheerful",
+	"Certain"
+];
+
+let filteredEndEmotions = endEmotions;
+
+window.onload = () => {
+	start();
+};
+
+function start() {
+	clearPage();
+	let startHtml = "";
+	startHtml =
+		startHtml +
+		`<div class="emotion-selector-container w3-animate-opacity">
+								<div class="question-prompt">Right now I feel:</div>
+								<div class="emotion-options">`;
+
+	emotions.forEach(emotion => {
+		startHtml =
+			startHtml +
+			`<div class="emotion" onclick="onStartEmotionSelected('${emotion}')">${emotion}</div>`;
+	});
+
+	startHtml = startHtml + `</div></div>`;
+	document.getElementById("content-container").innerHTML = startHtml;
+	//foreach emotion create an emotion
+}
+
+function onStartEmotionSelected(startEmotion) {
+	let html = `<div>Right now I feel ${startEmotion}.</div>`;
+	html =
+		html +
+		`<div class="emotion-selector-container w3-animate-opacity">
+								<div class="question-prompt">But I want to feel:</div>
+								<div class="emotion-options">`;
+
+	filteredEndEmotions = filteredEndEmotions.filter(x => x != startEmotion);
+	filteredEndEmotions.forEach(endEmotion => {
+		html =
+			html +
+			`<div class="emotion" onclick="onEndEmotionSelected('${startEmotion}', '${endEmotion}')">${endEmotion}</div>`;
+	});
+
+	html = html + `</div></div>`;
+
+	document.getElementById("content-container").innerHTML = html;
+}
+
+function onEndEmotionSelected(startEmotion, endEmotion) {
+	let html = `<div class="w3-animate-opacity"><div>Right now I feel ${startEmotion}.</div><div>But I want to feel ${endEmotion}.</div></div>`;
+
+	document.getElementById("content-container").innerHTML = html;
+}
+
+function clearPage() {
+	document.getElementById("content-container").innerHTML = "";
+	filteredEndEmotions = endEmotions;
+}
